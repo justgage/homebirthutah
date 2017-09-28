96a446633ad5d3fa92f7916ee296864661424ed2
Gage Peterson <justgage@gmail.com>
2017-09-27 23:06:19 -0600

    spice it up

--- dato.config.js
+++ dato.config.js
@@ -1,62 +1,58 @@
 const inspect = x => JSON.stringify(x, null, 2);
-const htmlTag = require("html-tag");
+const htmlTag = require('html-tag');
 
 const toHtml = tags =>
   tags
-  .map(({
-      tagName,
-      attributes,
-      content
-    }) =>
-    htmlTag(tagName, attributes, content)
-  )
-  .join("");
+    .map(({ tagName, attributes, content }) =>
+      htmlTag(tagName, attributes, content)
+    )
+    .join('');
 
-const toSlug = str =>
-  str.trim()
-  .replace(/[^a-zA-Z0-9]+/g, '-')
-  .toLowerCase();
+const toSlug = str => str.trim().replace(/[^a-zA-Z0-9]+/g, '-').toLowerCase();
 
 // dato.config.js
 module.exports = (dato, root, i18n) => {
-  const {
-    aboutPage,
-    midwives,
-    tabs
-  } = dato;
+  const { home, midwives, tabs } = dato;
 
-  console.log("Collections:", Object.keys(dato.collectionsByType));
+  console.log('Collections:', Object.keys(dato.collectionsByType));
 
-  root.directory("content", content => {
+  root.directory('content', content => {
+    content.createPost(`/_index.md`, 'yaml', {
+      frontmatter: {
+        title: '',
+        seoMetaTags: toHtml(home.seoMetaTags)
+      },
+      content: home.content
+    });
 
+    // ---------- lising type (midwife) ----------
     tabs.map(tab => {
-
       // make a new folder for each tab
       content.directory(tab.slug, dir => {
-
-        // for each of the listings make a page
+        // ---------- listing (Karla) ----------
         tab.listings.map((listing, i) => {
-          dir.createPost(`/${toSlug(listing.name)}.md`,
-            "yaml", {
-              frontmatter: {
-                title: listing.name,
-                photo: listing.picture && listing.picture
-                  .url({
-                    auto: "compress",
+          dir.createPost(`/${toSlug(listing.name)}.md`, 'yaml', {
+            frontmatter: {
+              title: listing.name,
+              photo:
+                listing.picture &&
+                  listing.picture.url({
+                    auto: 'compress',
                     w: 300,
                     fm: 'jpg'
                   }),
-                type: "tab",
-                email: listing.email,
-                phone: listing.phone,
-                websiteUrl: listing.website,
-                seoMetaTags: toHtml(listing.seoMetaTags),
-                weight: i,
-              },
-              content: listing.description
-            });
-        })
-
+              type: 'tab',
+              category: tab.title,
+              categorySlug: tab.slug,
+              email: listing.email,
+              phone: listing.phone,
+              websiteUrl: listing.website,
+              seoMetaTags: toHtml(listing.seoMetaTags),
+              weight: i
+            },
+            content: listing.description
+          });
+        });
       });
 
       console.log(JSON.stringify(tab.toMap(), null, 4));
